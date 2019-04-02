81e7b16cab2553e138b579f6b8ff75b2decbc5fa
Jiang Chong Yang <1174295440@qq.com>
2019-04-02 19:34:33 +0800

    sdf

--- src/main.js
+++ src/main.js
@@ -1,14 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { router } from './router'
-
 Vue.config.productionTip = false
 
 import { SwitchVbt, SwiperWrapperVbt, SwiperItemVbt } from '@npm/BT-vue-base'
-
-
-console.log('abc', { SwitchVbt, SwiperWrapperVbt, SwiperItemVbt })
-
 Vue.use(SwitchVbt)
 Vue.use(SwiperWrapperVbt)
 Vue.use(SwiperItemVbt)
@@ -19,18 +14,3 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
-
-
-
-
-var str = 'asdasasaasasasas??from=singlemessage&isappinstalled=0#/?url=https%3A%2F%2Fapi.guanliyuangong.com%2Fweixin%2Fmch-daily%3Fdata%3DYjg4MDdlMTFhODJlNDk5MzkyMjkzZTk1NjU2NzAwMWNmOGM3YmVlYWYwZDk4NmRmMDg5NWEyMjgzNTBjYWM0MmI4OTBkMmYyOWFiZDU4MWMxMjczOGNmMDhmNDNmMGNm'
-
-var arr = str.match(/\?[^?]*(?=(\?)?)/g)
-let useStr = ''
-if(arr) {
-  for(var a=0; a<arr.length; a++) {
-    var item = arr[a]
-    useStr += item.replace(/\#\/{0,}/g, '').replace(/\?[?]{0,}\??/, '&')
-  }
-}
-console.log('useStr', useStr)
