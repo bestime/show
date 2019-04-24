ebff1e37adc0db2d6fda736357a29d43f1803c6b
bestime <1174295440@qq.com>
2019-04-24 19:00:25 +0800

    sdf

--- src/main.js
+++ src/main.js
@@ -17,9 +17,12 @@ new Vue({
 
 
 
-var str = '<div class="one"><div class="h100">DESKTOP-GC4U8VE号机顾客离开打扫提醒<span class="jk">，请及时整理清洁卫生！</span></div></div>,bestime号机123，lynn号机'
+const arr = [
+  { num: 1 },
+  { num: 2 },
+  { num: 3 },
+]
 
-str = str.replace(/((,|，|\>)[^>，,]*号机)/g, function (g) {
-  return g.replace(/([^>，,]*号机)/g, '<b style="color:orange;">$1</b>')
-})
-console.log(str)
+console.log('reduce', arr.reduce((total, item) => {
+  return total + item.num
+}, 0))
